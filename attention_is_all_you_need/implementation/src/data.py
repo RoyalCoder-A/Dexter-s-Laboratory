db53d43291894f52c74a89fb88a27c82c2dc05b4
RoyalCoder-A <hendianarmin@gmail.com>
2025-01-12 02:36:03 +0330

    Update dataset handling and add MPS check in main script
    Improved dataset initialization by setting a custom download path and adding support for language fields in `get_dataset`. Updated `.gitignore` to exclude the new data directory. Added a check for Metal Performance Shaders availability in the main script for debugging purposes.

--- attention_is_all_you_need/implementation/src/data.py
+++ attention_is_all_you_need/implementation/src/data.py
@@ -1,7 +1,18 @@
+from pathlib import Path
+
 import torchtext
-from torch.utils.data import Dataset
 
 
-def get_dataset() -> Dataset:
-    dataset = torchtext.datasets.WMT14()
+def get_dataset():
+    data_path = Path(__file__).parent / "data"
+    if not data_path.exists():
+        torchtext.datasets.WMT14.download(root=str(data_path))
+    dataset = torchtext.datasets.WMT14(
+        path=str(data_path),
+        exts=("en", "de"),
+        fields=[
+            ("src", torchtext.data.Field(lower=True)),
+            ("trg", torchtext.data.Field(lower=True)),
+        ],
+    )
     return dataset
--- attention_is_all_you_need/implementation/src/main.py
+++ attention_is_all_you_need/implementation/src/main.py
@@ -1,4 +1,7 @@
+import torch
+
 from attention_is_all_you_need.implementation.src.data import get_dataset
 
 if __name__ == "__main__":
+    print(torch.mps.is_available())
     print(get_dataset())
