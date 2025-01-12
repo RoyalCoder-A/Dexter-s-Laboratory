810829a5b6c7102a5f90dbedd098d6e6ae42b4ea
Armin Hendian <hendianarmin@gmail.com>
2025-01-12 16:45:52 +0330

    Add attention is all you need summary

--- attention_is_all_you_need/implementation/src/main.py
+++ attention_is_all_you_need/implementation/src/main.py
@@ -1,7 +1,4 @@
 import torch
 
-from attention_is_all_you_need.implementation.src.data import get_dataset
-
 if __name__ == "__main__":
     print(torch.mps.is_available())
-    print(get_dataset())
