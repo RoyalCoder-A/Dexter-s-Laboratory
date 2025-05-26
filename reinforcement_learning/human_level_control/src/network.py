8b6f59bed6fc20679297645acc8961a09976637f
Armin Hendian <hendianarmin@gmail.com>
2025-05-26 04:57:07 +0330

    ✨ fix: add missing ReLU activations in the convolutional layers for improved non-linearity

--- reinforcement_learning/human_level_control/src/network.py
+++ reinforcement_learning/human_level_control/src/network.py
@@ -6,8 +6,11 @@ class Network(torch.nn.Module):
         super().__init__(*args, **kwargs)
         self.sequential = torch.nn.Sequential(
             torch.nn.Conv2d(input_shape[0], 32, (8, 8), 4),
+            torch.nn.ReLU(),
             torch.nn.Conv2d(32, 64, (4, 4), 2),
+            torch.nn.ReLU(),
             torch.nn.Conv2d(64, 64, (3, 3), 1),
+            torch.nn.ReLU(),
             torch.nn.Flatten(),
             torch.nn.Linear(3136, 512),
             torch.nn.ReLU(),
