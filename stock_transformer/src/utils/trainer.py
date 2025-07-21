d351656f5661e47fdb243a3051d02faa4a7ce3cc
armin hendian <hendianarmin@gmail.com>
2025-07-21 05:45:30 +0330

    ✨ feat: implement TransformerModel with PreLayer, Encoder, and Decoder components

---
+++ stock_transformer/src/utils/trainer.py
@@ -0,0 +1,130 @@
+from pathlib import Path
+from typing import Literal
+
+import torch
+import tqdm
+from torchmetrics.regression import R2Score
+from torch.utils.tensorboard.writer import SummaryWriter
+
+from stock_transformer.src.utils.transformer_model import TransformerModel
+
+
+class Trainer:
+    def __init__(
+        self,
+        transformer_model: TransformerModel,
+        batch_size: int,
+        epochs: int,
+        warmup_steps: int,
+        d_model: int,
+        device: Literal["cpu", "cuda", "mps"],
+        train_dl: torch.utils.data.DataLoader,
+        valid_dl: torch.utils.data.DataLoader,
+        summary_writer: SummaryWriter,
+        checkpoint_path: Path,
+    ) -> None:
+        self.transformer_model = transformer_model
+        self.warmup_steps = warmup_steps
+        self.d_model = d_model
+        self.batch_size = batch_size
+        self.epochs = epochs
+        self.device = device
+        self.train_dl = train_dl
+        self.valid_dl = valid_dl
+        self.loss_fn = torch.nn.CrossEntropyLoss().to(device)
+        self.validation_metric = R2Score().to(device)
+        self.optimizer = torch.optim.Adam(
+            self.transformer_model.parameters(), betas=(0.9, 0.98), eps=1e-9
+        )
+        self.summary_writer = summary_writer
+        self.current_step = 0
+        self.checkpoint_path = checkpoint_path
+
+    def train(self):
+        self.current_step = 0
+        best_loss = float("inf")
+        for i in range(self.epochs):
+            print(f"Epoch {i+1} from {self.epochs}")
+            train_loss = self._train_step()
+            test_loss, test_val = self._test_step()
+            self.summary_writer.add_scalar("train_loss", train_loss, i)
+            self.summary_writer.add_scalar("test_loss", test_loss, i)
+            self.summary_writer.add_scalar("test_val", test_val, i)
+            print(f"Train loss: {train_loss:.4f}")
+            print(f"Test loss: {test_loss:.4f}")
+            print(f"Test val: {test_val:.4f}")
+            if test_loss < best_loss:
+                best_loss = test_loss
+                self._save_checkpoint()
+            print("=" * 80)
+
+    def _train_step(self) -> float:
+        self.transformer_model.train()
+        losses: list[float] = []
+        pbar = tqdm.tqdm(self.train_dl)
+        for encoder_input, decoder_input, tgt in pbar:
+            self.optimizer.zero_grad()
+            pred_logits = self.transformer_model(
+                encoder_input.to(self.device),
+                decoder_input.to(self.device),
+            )
+            pred_logits = pred_logits.reshape(-1, pred_logits.size(-1))
+            tgt = tgt.to(self.device).reshape(-1)
+            loss: torch.Tensor = self.loss_fn(pred_logits, tgt)
+            loss_number = loss.detach().cpu().item()
+            losses.append(loss_number)
+            loss.backward()
+            self.optimizer.step()
+            lr = self._update_lr()
+            self.summary_writer.add_scalar("learning_rate", lr, self.current_step)
+            pbar.set_description(f"loss: {loss_number:.4f} lr: {lr:.6f}")
+        return sum(losses) / len(losses)
+
+    def _test_step(self) -> tuple[float, float]:
+        self.transformer_model.eval()
+        with torch.inference_mode():
+            losses: list[float] = []
+            val_scores: list[float] = []
+            pbar = tqdm.tqdm(self.valid_dl)
+            for encoder_input, decoder_input, tgt in pbar:
+                pred_logits = self.transformer_model(
+                    encoder_input.to(self.device),
+                    decoder_input.to(self.device),
+                )
+                loss: torch.Tensor = self.loss_fn(
+                    pred_logits.reshape(-1, pred_logits.size(-1)),
+                    tgt.to(self.device).reshape(-1),
+                )
+                loss_number = loss.detach().cpu().item()
+                losses.append(loss_number)
+                validation = self.validation_metric(pred_logits, tgt)
+                pbar.set_description(f"loss: {loss_number:.4f} val: {validation:.4f}")
+                val_scores.append(validation)
+            return sum(losses) / len(losses), sum(val_scores) / len(val_scores)
+
+    def _calculate_validation(
+        self, pred_logits: torch.Tensor, tgt_ids: torch.Tensor
+    ) -> float:
+        pred_ids = torch.argmax(pred_logits, dim=-1).detach().cpu().numpy()
+        pred_strs = self.tokenizer.decode_batch(pred_ids)
+        tgt_strs = self.tokenizer.decode_batch(tgt_ids.detach().cpu().numpy())
+        bleu_score = self.validation_metric(
+            [pred_str for pred_str in pred_strs], [[tgt_str] for tgt_str in tgt_strs]
+        )
+        return bleu_score
+
+    def _update_lr(self) -> float:
+        self.current_step += 1
+        lr = self.d_model**-0.5 * min(
+            self.current_step**-0.5, self.current_step * self.warmup_steps**-1.5
+        )
+        for param in self.optimizer.param_groups:
+            param["lr"] = lr
+        return lr
+
+    def _save_checkpoint(self) -> None:
+        self.checkpoint_path.parent.mkdir(parents=True, exist_ok=True)
+        torch.save(
+            self.transformer_model.state_dict(),
+            self.checkpoint_path,
+        )
---
+++ stock_transformer/src/utils/transformer_model.py
@@ -0,0 +1,53 @@
+import torch
+
+from attention_is_all_you_need.src.utils.decoder import Decoder
+from attention_is_all_you_need.src.utils.encoder import Encoder
+from stock_transformer.src.utils.pre_layer import PreLayer
+
+
+class TransformerModel(torch.nn.Module):
+
+    def __init__(
+        self,
+        feature_size: int,
+        d_model: int,
+        n: int,
+        dff: int,
+        n_heads: int,
+        p_dropout: float,
+    ) -> None:
+        super().__init__()
+        self.pre_layer = PreLayer(feature_size, d_model)
+        self.encoder = Encoder(n, d_model, dff, n_heads, p_dropout)
+        self.decoder = Decoder(n, d_model, dff, n_heads, p_dropout)
+        self.head = torch.nn.Linear(d_model, feature_size)
+        self._reset_parameters()
+
+    def _reset_parameters(self) -> None:
+        """Initialize the transformer parameters."""
+        for p in self.parameters():
+            if p.dim() > 1:
+                torch.nn.init.xavier_uniform_(p)
+
+    def forward(
+        self, encoder_input: torch.Tensor, decoder_input: torch.Tensor
+    ) -> torch.Tensor:
+        src_mask, tgt_mask = self.generate_mask(encoder_input, decoder_input)
+        encoder_input = self.pre_layer(encoder_input)
+        encoder_output = self.encoder(encoder_input, src_mask)
+        decoder_input = self.pre_layer(decoder_input)
+        decoder_output = self.decoder(decoder_input, encoder_output, src_mask, tgt_mask)
+        return self.head(decoder_output)
+
+    def generate_mask(self, src: torch.Tensor, tgt: torch.Tensor):
+        device = src.device
+        src_mask = (src != 0).unsqueeze(1).unsqueeze(2).to(device)
+        tgt_mask = (tgt != 0).unsqueeze(1).unsqueeze(3).to(device)
+        seq_length = tgt.size(1)
+        nopeak_mask = (
+            (1 - torch.triu(torch.ones(1, seq_length, seq_length), diagonal=1))
+            .bool()
+            .to(device)
+        )
+        tgt_mask = tgt_mask & nopeak_mask
+        return src_mask, tgt_mask
